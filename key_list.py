414db7e712404e63c3ac6a3509b890ae33c2ff88
LiamMcFadden <liamm18@vt.edu>
2020-06-30 17:22:36 -0400

    Key/mouse input

---
+++ key_list.py
@@ -0,0 +1,16 @@
+# This solution is from here -> Box Of Hats (https://github.com/Box-Of-Hats )
+# Modified to to only look for the keys in key_list
+
+import win32api as wapi
+import time
+
+key_list = [0x01, 0x02, 0x57, 0x41, 0x53, 0x44, 0x10]
+key_dic = {0x01:'Left Click', 0x02:'Right Click', 0x57:'w', 0x41:'a', 0x53:'s', 0x44:'d', 0x10:'Shift'}
+
+def key_check():
+    keys = []
+    for key in key_list:
+        if wapi.GetAsyncKeyState((key)):
+            keys.append(key_dic[key])
+
+    return keys
