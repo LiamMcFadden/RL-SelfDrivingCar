297f5327fe5902da050db5d0ca3a5a7f9148d69a
LiamMcFadden <liamm18@vt.edu>
2020-07-05 23:55:27 -0400

    ball indentification progress

--- process_image.py
+++ process_image.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
+import os
+import time
 
 def process_image(img):
+    curr = time.time()
+    cwd = os.getcwd()
     img = cv2.GaussianBlur(img, (5, 5), 0)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #print("Elapsed time: {}".format(time.time() - curr))
 
 
     return img
--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from screen_cap import screen_cap
 from process_image import process_image
 import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
 
-img = screen_cap()
-img = process_image(img)
+img = cv2.imread('./img/apple.jpeg')
+bbox, label, conf = cv.detect_common_objects(img)
+output_image = draw_bbox(img, bbox, label, conf)
+plt.imshow(output_image)
+plt.show()
 
-cv2.imshow("test", img)
-cv2.waitKey(0)
+
